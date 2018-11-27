2202cc918cbf7df7f0976d42dbef5deae68664e9
白泽 <353311852@qq.com>
2018-11-28 00:28:48 +0800

    学习.netc# 7.0 的相关语法

---
+++ MyApp/MyApp/NewAttribute.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+	public class NewAttribute
+	{
+		private NewAttribute()
+		{
+
+		}
+
+		public static readonly NewAttribute Intace = new NewAttribute();
+
+		/// <summary>
+		/// 1. out-variables(Out变量)
+		/// </summary>
+		/// <param name="result"></param>
+		public void OutParams(out int result)
+		{
+			result = 0;
+			result += 2;
+		}
+
+		/// <summary>
+		/// 2.Tuples(元组)
+		/// </summary>
+		/// <returns></returns>
+		public (string xm, string lL, string hanmm) GetFullName()
+		{
+			return ("小明", "李雷", "韩梅梅");
+		}
+
+		/// <summary>
+		/// Pattern Matching(匹配模式)
+		/// </summary>
+		/// <returns></returns>
+		public int PatternMatching()
+		{
+			object a = 1;
+			int d = 0;
+			if (a is int c)
+			{
+				 d = c + 10;
+			}
+			return d;
+		}
+
+		public dynamic PatternMatching(object a)
+		{
+			dynamic data;
+			switch (a)
+			{
+				case int b:
+					data = b++;
+					break;
+				case string c:
+					data = c + "HelloWorld";
+					break;
+				default:
+					data = null;
+					break;
+			}
+			return data;
+		}
+
+		/// <summary>
+		/// 4.ref locals and returns(局部变量和引用返回)
+		/// </summary>
+		/// <param name="arr"></param>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public ref int GetByIndex(int[] arr, int index) => ref arr[index];
+
+		public string IsNull(string str) => str ?? throw new Exception("字符串不能为空");
+	}
+}
--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -10,7 +10,16 @@ namespace MyApp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("修改后");
+			#region C#7.0新特性
+			NewAttribute.Intace.OutParams(out int result);
+			var (xm, lL, hanmm) = NewAttribute.Intace.GetFullName();
+			object a = "1";
+			dynamic res = NewAttribute.Intace.PatternMatching(a);
+			int[] arr = new int[] { 1, 2, 3, 4, 5 };
+			ref int b =ref NewAttribute.Intace.GetByIndex(arr, 1);
+			//C#7.0异常处理机制
+			NewAttribute.Intace.IsNull("");
+			#endregion
 		}
 	}
 }
